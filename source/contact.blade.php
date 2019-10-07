c269bcb604e0df26993ca53b6638fdc902cc7025
Anik1199 <anik@lineageos.org>
2019-10-08 00:12:48 +0600

    Add contact form using netlify

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -10,12 +10,36 @@
 @section('body')
 
 <section>
-    <h1 class="text-center">Get in touch</h1>
-    <p class="text-center text-3xl">
-      <a href="mailto:anik@lineageos.org">
-        &#8594; Send me an email.
-      </a>
-    </p>
+    <h1>Get in touch</h1>
+    <div class=pt-4>
+      <form class="w-full max-w-lg" name="contact" method="POST" data-netlify="true">
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+              Your Name
+            </label>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="" name="name">
+          </div>
+          <div class="w-full md:w-1/2 px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+              Your Email
+            </label>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="email" placeholder="" name="email">
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+              Message
+            </label>
+            <textarea class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" name="message"></textarea>
+          </div>
+          <div class="py-4 px-4">
+            <button class="bg-blue-dark hover:bg-blue-darker text-white font-bold py-3 px-6 rounded" type="submit">Send</button>
+          </div>
+        </div>
+      </form>
+    </div>
   </section>
 
 @stop
