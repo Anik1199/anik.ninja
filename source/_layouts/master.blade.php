7cf8d79e69ab75bd082d95c158fb08b88afc362d
Anik1199 <anik@lineageos.org>
2019-10-08 20:59:58 +0600

    Add Google Analytics and Tag manager

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -32,17 +32,24 @@
 
         @if ($page->production)
             <!-- Global site tag (gtag.js) - Google Analytics -->
-            <script async src=""></script>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-149583726-1"></script>
             <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'UA-89589431-1', { 'optimize_id': ''});
+            gtag('config', 'UA-149583726-1');
             </script>
+        @endif
 
-
-            
+        @if ($page->production)
+            <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NNJ4KXN');</script>
+            <!-- End Google Tag Manager -->
         @endif
 
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
@@ -53,7 +60,7 @@
 
         @if ($page->production)
         <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src=""
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NNJ4KXN"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <!-- End Google Tag Manager (noscript) -->
         @endif
