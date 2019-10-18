d23357bbceaecaec0b40f97038e525cb543aa567
Anik1199 <anik@lineageos.org>
2019-10-18 23:46:35 +0600

    Add custom form success page

---
+++ source/success.blade.php
@@ -0,0 +1,11 @@
+@extends('_layouts.master')
+
+@section('body')
+    <div class="flex flex-col items-center text-grey-darker mt-32">
+        <h1 class="text-3xl leading-none mb-2">Thank You!</h1>
+
+        <h2 class="text-3xl font-light">I will get back to you soon.</h2>
+
+        <hr class="block w-full max-w-sm mx-auto border my-8">
+    </div>
+@endsection
