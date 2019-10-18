32c691127cf1456be405921000f77f0affe6df3a
Anik1199 <anik@lineageos.org>
2019-10-18 23:54:44 +0600

    Fix font size on 404 page

--- source/404.blade.php
+++ source/404.blade.php
@@ -2,9 +2,9 @@
 
 @section('body')
     <div class="flex flex-col items-center text-grey-darker mt-32">
-        <h1 class="text-6xl font-light leading-none mb-2">404</h1>
+        <h1 class="text-5xl leading-none mb-2">404</h1>
 
-        <h2 class="text-3xl">Page not found.</h2>
+        <h2 class="text-3xl font-light">Page not found.</h2>
 
         <hr class="block w-full max-w-sm mx-auto border my-8">
     </div>
